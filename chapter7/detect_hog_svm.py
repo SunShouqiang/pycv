3237a23bce0ca828a97845c4f65bad0ef160d841
SunShouqiang <48129698+SunShouqiang@users.noreply.github.com>
2019-03-06 20:20:53 +0800

    Update detect_hog_svm.py

--- chapter7/detect_hog_svm.py
+++ chapter7/detect_hog_svm.py
@@ -1,66 +1,65 @@
 import cv2
 import numpy as np
-from os.path import join
 
-datapath = "/home/d3athmast3r/dev/python/CarData/TrainImages/"
-def path(cls,i):
-  return "%s/%s%d.pgm"  % (datapath,cls,i+1)
-
-pos, neg = "pos-", "neg-"
-
+#创建两个SIFT实例，一个提取关键点，一个是提取特征
 detect = cv2.xfeatures2d.SIFT_create()
 extract = cv2.xfeatures2d.SIFT_create()
 
 flann_params = dict(algorithm = 1, trees = 5)
-matcher = cv2.FlannBasedMatcher(flann_params, {})
+matcher = cv2.FlannBasedMatcher(flann_params, {})       #Flann匹配
 
-bow_kmeans_trainer = cv2.BOWKMeansTrainer(40)
+bow_kmeans_trainer = cv2.BOWKMeansTrainer(40)          #BOW训练器       指定簇数为40
 extract_bow = cv2.BOWImgDescriptorExtractor(extract, matcher)
 
 def extract_sift(fn):
   im = cv2.imread(fn,0)
   return extract.compute(im, detect.detect(im))[1]
+
+#m = cv2.imread('CarData/TrainImages/pos-' + str(1 +1)+'.pgm')
+#cv2.imshow('hel',m)
+
+for i in range(8):            # 每个类都从数据集中读取八张图像（八个正样例，和八个负样例）
+  bow_kmeans_trainer.add(extract_sift('CarData/TrainImages/pos-' + str(i +1) +'.pgm'))
+  bow_kmeans_trainer.add(extract_sift('CarData/TrainImages/neg-' + str(i +1) +'.pgm'))
+  # print('a')
   
-for i in range(8):
-  bow_kmeans_trainer.add(extract_sift(path(pos,i)))
-  bow_kmeans_trainer.add(extract_sift(path(neg,i)))
-  
-voc = bow_kmeans_trainer.cluster()
+voc = bow_kmeans_trainer.cluster()          #创建视觉单词词汇调用cluster函数，执行k-means分类并返回词汇
 extract_bow.setVocabulary( voc )
 
 def bow_features(fn):
   im = cv2.imread(fn,0)
+  # print('d')
   return extract_bow.compute(im, detect.detect(im))
 
 traindata, trainlabels = [],[]
-for i in range(20): 
-  traindata.extend(bow_features(path(pos, i))); trainlabels.append(1)
-  traindata.extend(bow_features(path(neg, i))); trainlabels.append(-1)
+for i in range(100):            # 检测范围越大越准确
+  traindata.extend(bow_features('CarData/TrainImages/pos-' + str(i +1)+'.pgm')); trainlabels.append(1)        #（1代表正匹配，-1代表负匹配）
+  # print('c')
+  traindata.extend(bow_features('CarData/TrainImages/neg-' + str(i +1)+'.pgm')); trainlabels.append(-1)
+  # print('b')
 
-svm = cv2.ml.SVM_create()
+svm = cv2.ml.SVM_create()         #创建SVM实例
 svm.train(np.array(traindata), cv2.ml.ROW_SAMPLE, np.array(trainlabels))
 
 def predict(fn):
-  f = bow_features(fn);  
+  f = bow_features(fn)
   p = svm.predict(f)
-  print fn, "\t", p[1][0][0]
+  print(fn, "\t", p[1][0][0])
   return p
-  
-car, notcar = "/home/d3athmast3r/dev/python/study/images/car.jpg", "/home/d3athmast3r/dev/python/study/images/bb.jpg"
+
+# again placeholder paths
+car ="car.jpg"      #测试项
 car_img = cv2.imread(car)
-notcar_img = cv2.imread(notcar)
 car_predict = predict(car)
-not_car_predict = predict(notcar)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 if (car_predict[1][0][0] == 1.0):
   cv2.putText(car_img,'Car Detected',(10,30), font, 1,(0,255,0),2,cv2.LINE_AA)
 
-if (not_car_predict[1][0][0] == -1.0):
-  cv2.putText(notcar_img,'Car Not Detected',(10,30), font, 1,(0,0, 255),2,cv2.LINE_AA)
+if (car_predict[1][0][0] == -1.0):
+  cv2.putText(car_img,'Car Not Detected',(10,30), font, 1,(0,0, 255),2,cv2.LINE_AA)
 
-cv2.imshow('BOW + SVM Success', car_img)
-cv2.imshow('BOW + SVM Failure', notcar_img)
+cv2.imshow('BOW + SVM Result', car_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
